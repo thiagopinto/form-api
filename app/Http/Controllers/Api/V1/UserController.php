fd2186ff8d00d8d0cb631b0b1610db6f5a837665
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2021-09-15 11:39:07 -0300

    debug register user

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -62,7 +62,7 @@ class UserController extends Controller
             'name' => 'required|string|max:255',
             'phone' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => ['required', 'passwordConfirmation', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
